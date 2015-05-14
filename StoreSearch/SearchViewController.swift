a9ee805154422e9eac7c780e88b4d9207cf3a913
Donald Lee <donaldlee50@gmail.com>
2015-05-14 16:53:58 -0700

    Doing fake searches

--- StoreSearch/SearchViewController.swift
+++ StoreSearch/SearchViewController.swift
@@ -8,10 +8,14 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class SearchViewController: UIViewController {
 
+    @IBOutlet weak var searchBar: UISearchBar!
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableView.contentInset = UIEdgeInsets(top: 64, left: 0, bottom: 0, right: 0)
         // Do any additional setup after loading the view, typically from a nib.
     }
 
