dd46fae23537920bfb713937f4d072fc282b71d4
Donald Lee <donaldlee50@gmail.com>
2015-05-14 17:22:56 -0700

    table displaying and fake results

--- StoreSearch/SearchViewController.swift
+++ StoreSearch/SearchViewController.swift
@@ -13,6 +13,8 @@ class SearchViewController: UIViewController {
     @IBOutlet weak var searchBar: UISearchBar!
     @IBOutlet weak var tableView: UITableView!
     
+    var searchResults = [String]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.contentInset = UIEdgeInsets(top: 64, left: 0, bottom: 0, right: 0)
@@ -24,6 +26,32 @@ class SearchViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
 }
+
+extension SearchViewController: UISearchBarDelegate {
+    func searchBarSearchButtonClicked(searchBar: UISearchBar) {
+        searchResults = [String]()
+        for i in 0...2 {
+            searchResults.append(String(format: "Fake Result %d for '%@'", i,searchBar.text))
+        }
+        tableView.reloadData()
+    }
+}
+
+extension SearchViewController: UITableViewDataSource {
+    func tableView(tableView: UITableView,
+        numberOfRowsInSection section: Int) -> Int {
+        return searchResults.count
+    }
+        
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cellIdentifier = "SearchResultCell"
+        var cell = tableView.dequeueReusableCellWithIdentifier(cellIdentifier) as! UITableViewCell!
+        cell = UITableViewCell(style: .Default, reuseIdentifier: cellIdentifier)
+        cell.textLabel!.text = searchResults[indexPath.row]
+            
+        return cell
+    }
+}
+extension SearchViewController: UITableViewDelegate { }
 
