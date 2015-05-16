df79b6afdbe26222c7528f7e3c87f91e5596c69c
Donald Lee <donaldlee50@gmail.com>
2015-05-16 03:30:19 -0700

    Putting the data into the Detail pop-up

--- DetailViewController.swift
+++ DetailViewController.swift
@@ -10,6 +10,14 @@ import UIKit
 
 class DetailViewController: UIViewController {
     
+    @IBOutlet weak var popupView: UIView!
+    @IBOutlet weak var artworkImageView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var artistNameLabel: UILabel!
+    @IBOutlet weak var kindLabel: UILabel!
+    @IBOutlet weak var genreLabel: UILabel!
+    @IBOutlet weak var priceButton: UIButton!
+    
     required init(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
         modalPresentationStyle = .Custom
@@ -19,7 +27,12 @@ class DetailViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        view.tintColor = UIColor(red: 20/255, green: 160/255, blue: 160/255, alpha: 1)
+        popupView.layer.cornerRadius = 10
+    
+        let gestureRecognizer = UITapGestureRecognizer(target: self, action: Selector("close"))
+        gestureRecognizer.cancelsTouchesInView = false
+        gestureRecognizer.delegate = self view.addGestureRecognizer(gestureRecognizer)
     }
 
     override func didReceiveMemoryWarning() {
@@ -37,5 +50,13 @@ class DetailViewController: UIViewController {
 extension DetailViewController: UIViewControllerTransitioningDelegate {
     func presentationControllerForPresentedViewController(presented: UIViewController, presentingViewController presenting: UIViewController!, sourceViewController source: UIViewController) -> UIPresentationController? {
         return DimmingPresentationController(presentedViewController:presented, presentingViewController: presenting)
+    }
+}
+
+
+extension DetailViewController: UIGestureRecognizerDelegate {
+    // Allow users to dismiss popup by tapping anywhere outside it
+    func gestureRecognizer(gestureRecognizer: UIGestureRecognizer, shouldReceiveTouch touch: UITouch) -> Bool {
+        return (touch.view === view)
     }
 }
